e6791868c8cca36584cf39129cbf7fde241c0df1
LucasArona <LucasArona@users.noreply.github.com>
2018-11-10 18:51:40 +0100

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -42,24 +42,24 @@ module.exports = (grunt) => {
       },
     },
 
-    babel: {
-      options: {
-        sourceMap: true,
-        presets: ['es2015'],
-        plugins: ['transform-es2015-modules-systemjs', 'transform-es2015-for-of'],
-      },
-      dist: {
-        files: [{
-          cwd: 'src',
-          expand: true,
-          src: ['*.js'],
-          dest: 'dist',
-          ext: '.js'
-        }]
-      },
-    },
+  //   babel: {
+  //     options: {
+  //       sourceMap: true,
+  //       presets: ['es2015'],
+  //       plugins: ['transform-es2015-modules-systemjs', 'transform-es2015-for-of'],
+  //     },
+  //     dist: {
+  //       files: [{
+  //         cwd: 'src',
+  //         expand: true,
+  //         src: ['*.js'],
+  //         dest: 'dist',
+  //         ext: '.js'
+  //       }]
+  //     },
+  //   },
 
   });
 
-  grunt.registerTask('default', ['clean', 'copy:src_to_dist', 'copy:pluginDef', 'copy:img_to_dist', 'copy:externals', 'babel']);
+  grunt.registerTask('default', ['clean', 'copy:src_to_dist', 'copy:pluginDef', 'copy:img_to_dist', 'copy:externals']);
 };
