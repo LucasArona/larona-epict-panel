0c3ee15f914e98766e885aff5f22132e9438e04f
LucasArona <LucasArona@users.noreply.github.com>
2020-11-26 23:16:21 +0100

    Delete module.ts

--- src/module.ts
+++
@@ -1,7 +0,0 @@
-import { PanelPlugin } from '@grafana/data';
-import { SimpleOptions } from './types';
-import { SimplePanel as EpictPanel } from './EpictPanel';
-import { EpictEditor } from 'EpictEditor';
-import { MigHandler } from 'MigHandler';
-
-export const plugin = new PanelPlugin<SimpleOptions>(EpictPanel).setEditor(EpictEditor).setMigrationHandler(MigHandler);
