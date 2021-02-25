81f045e03b530d6ae3b0b819331c4457936eb63c
LucasArona <LucasArona@users.noreply.github.com>
2021-02-25 21:25:31 +0100

    v2.0.1 fixes #34

--- src/MigHandler.tsx
+++ src/MigHandler.tsx
@@ -7,38 +7,44 @@ class Opts implements SimpleOptions {
 }
 
 export const MigHandler = (panel: PanelModel<Partial<SimpleOptions>> | any): Partial<SimpleOptions> => {
-  console.log('Migrating from an older version', panel);
-  const options: SimpleOptions = new Opts();
-  options.bgURL = panel.bgURL;
-  options.boxes = [];
-  panel.boxes.forEach((oldBox: any) => {
-    console.log('Found a box to upgrade:', oldBox);
-    options.boxes.push({
-      angle: oldBox.angle,
-      color: oldBox.color,
-      colorHigh: oldBox.colorHigh,
-      colorLow: oldBox.colorLow,
-      colorMedium: oldBox.colorMedium,
-      decimal: oldBox.decimal,
-      fontSize: oldBox.fontsize,
-      hasOrb: oldBox.hasOrb,
-      orbHideText: oldBox.orbHideText,
-      orbLocation: oldBox.orbLocation,
-      orbSize: oldBox.orbSize,
-      prefixSize: oldBox.prefixSize,
-      serie: oldBox.serie,
-      suffixSize: oldBox.suffixSize,
-      text: oldBox.text,
-      suffix: oldBox.suffix,
-      prefix: oldBox.prefix,
-      thresholds: oldBox.thresholds,
-      blinkHigh: oldBox.blinkHigh,
-      blinkLow: oldBox.blinkLow,
-      isUsingThresholds: oldBox.usingThresholds,
-      xpos: oldBox.xpos,
-      ypos: oldBox.ypos,
-      url: oldBox.URL,
+  if (panel.boxes != null) {
+    console.log('Migrating from an older version', panel);
+    const options: SimpleOptions = new Opts();
+    options.bgURL = panel.bgURL;
+    options.boxes = [];
+    panel.boxes.forEach((oldBox: any) => {
+      console.log('Found a box to upgrade:', oldBox);
+      options.boxes.push({
+        angle: oldBox.angle,
+        color: oldBox.color,
+        colorHigh: oldBox.colorHigh,
+        colorLow: oldBox.colorLow,
+        colorMedium: oldBox.colorMedium,
+        decimal: oldBox.decimal,
+        fontSize: oldBox.fontsize,
+        hasOrb: oldBox.hasOrb,
+        orbHideText: oldBox.orbHideText,
+        orbLocation: oldBox.orbLocation,
+        orbSize: oldBox.orbSize,
+        prefixSize: oldBox.prefixSize,
+        serie: oldBox.serie,
+        suffixSize: oldBox.suffixSize,
+        text: oldBox.text,
+        suffix: oldBox.suffix,
+        prefix: oldBox.prefix,
+        thresholds: oldBox.thresholds,
+        blinkHigh: oldBox.blinkHigh,
+        blinkLow: oldBox.blinkLow,
+        isUsingThresholds: oldBox.usingThresholds,
+        xpos: oldBox.xpos,
+        ypos: oldBox.ypos,
+        url: oldBox.URL,
+      });
     });
-  });
-  return options;
+    return options;
+  } else {
+    //Nothing to do
+    console.log('MigHandler: Nothing to upgrade');
+    return panel.options;
+  }
 };
